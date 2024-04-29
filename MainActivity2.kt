5c7b5950423bc8a4a681be5973ab5b8ee56469f2
SIPHESIHLE1705 <162302326+SIPHESIHLE1705@users.noreply.github.com>
2024-04-29 21:36:31 +0000

    Update MainActivity2.kt

--- MainActivity2.kt
+++ MainActivity2.kt
@@ -7,6 +7,70 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        / Get the button and text views
+        val btnFeed =findViewById<Button>(R.id.btnFeed)
+        val btnClean =findViewById<Button>(R.id.btnClean)
+        val btnHappy =findViewById<Button>(R.id.btnPlay)
+        val txtHunger =findViewById<EditText>(R.id.txtHunger_value)
+        val txtHealth =findViewById<EditText>(R.id.txtHealth_value)
+        val txtHappy =findViewById<EditText>(R.id.txtCleanliness_value)
+        val petImage =findViewById<ImageView>(R.id.petImage)
+
+        // Set the initial text values
+        txtHunger.setText(petHunger.toString())
+        txtHealth.setText(petHealth.toString())
+        txtHappy.setText(petHealth.toString())
+
+        //Handle button clicks
+        btnFeed.setOnClickListener {
+
+            petHunger -= 10
+            petHealth += 10
+            petHunger += 5
+            txtHunger.setText(petHunger.toString())
+            animateImageChange(petImage, R.drawable.cat_eating)
+
+
+            btnClean.text = petCleanliness.toString()
+            petCleanliness += 10
+            petHealth += 10
+
+            btnClean.setText(petCleanliness)
+
+
+        }
+        btnClean.setOnClickListener {
+            petCleanliness += 10
+            petHealth += 10
+
+            txtHealth.setText(petCleanliness.toString())
+            animateImageChange(petImage, R.drawable.cat_cleaning)
+
+        }
+        btnHappy.setOnClickListener {
+            petHealth += 10
+            petHunger += 5
+            petCleanliness -= 5
+            txtHappy.setText(petHealth.toString())
+            animateImageChange(petImage, R.drawable.cat_playing)
+
+        }
+
+    }
+
+    private fun animateImageChange(imageView: ImageView, newImageResource: Int) {
+        val animate = AlphaAnimation(0.0f, 1.0f)
+        animate.duration = 500
+        animate.fillAfter = true
+        imageView.startAnimation(animate)
+        imageView.setImageResource(newImageResource)
+
+
+
+    }
+
+}
+
 
     }
 }
